52cd0458a6d2ac54125f0aa3d46571dfe347071c
Kate Gerber <kate@gerber.id.au>
2024-04-26 21:18:15 +1000

    working on game function, updates display after user input

--- src/Game.py
+++ src/Game.py
@@ -8,12 +8,14 @@ class Game:
         self._guesses = ""
         self._blanked_word = ""
         self._word_generator = RandomWord()
+        self._game_over = False
 
 
     # generates random word
     def generate_word(self):
         return self._word_generator.word(word_min_length=5, word_max_length=10)
 
+    # Getter
     def get_blanked_word(self):
         return self._blanked_word
 
@@ -30,8 +32,16 @@ class Game:
 
   # Goes through each letter and checks if the guessed letter is in word then displays blank if incorrect
 
-
+# starts the game
     def start_game(self):
+        #generates random word
         self._hangman_word = self.generate_word()
-        self.check_letters()
-        print(self._blanked_word)
+        # while game is playing
+        while self._game_over == False:
+            self.check_letters()
+            print(self._blanked_word)
+            user_letter = input ("Try guessing a letter.")
+            self._guesses += user_letter
+            
+            
+            #wip, still need to add number of guesses & set game over, plus other things.
