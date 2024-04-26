58afd712887042b58e34e298ab572db8355fb9d6
Kate Gerber <kate@gerber.id.au>
2024-04-26 16:44:58 +1000

    added comments

--- src/hangman_functions.py
+++ src/hangman_functions.py
@@ -1,27 +1,35 @@
 import os
 
+# Function to clear screen
+def clear_screen():
+    # this will clear the screen between menus
+    os.system("clear")
+
+# function to return to main menu after user says so
 def any_key_return_to_menu():
     # Wait for the user to input a key to acknowledge they have finished the help page
     user_input = input("Press any key to return to main menu.")
     # Clears the screen before returning to menu
-    os.system ("clear")
+    clear_screen()
 
+# Function to display at beginning of application
 def display_intro():
-    # this will clear the screen between menus
-    os.system("clear")
+    clear_screen()
     print("Welcome to Hangman!")
 
 # Function to display help info
 def display_help():
-    os.system ("clear")
+    clear_screen()
     print("This will print some help info for how to play the game.") 
     any_key_return_to_menu()
 
+# Function to display stats page
 def display_stats():
-    os.system ("clear")
+    clear_screen()
     print("Stats")
     any_key_return_to_menu()
 
+# Function to start the game
 def play_game():
-    os.system("clear")
+    clear_screen()
     print("")
--- src/run.sh
+++ src/run.sh
@@ -4,7 +4,8 @@
 python3 -m venv .venv
 source .venv/bin/activate
 
+# Packets to be installed
 pip3 install colored
 pip3 install wonderwords
-
+# Runs the program
 python3 application.py
