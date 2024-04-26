f1bf933915bbecbb3b5c1bef253d8438572277ee
Kate Gerber <kate@gerber.id.au>
2024-04-26 22:22:00 +1000

    added check user guess function, game over feature, &  error handling for user input

--- src/Game.py
+++ src/Game.py
@@ -9,6 +9,7 @@ class Game:
         self._blanked_word = ""
         self._word_generator = RandomWord()
         self._game_over = False
+        
 
 
     # generates random word
@@ -20,28 +21,53 @@ class Game:
         return self._blanked_word
 
 
-# Goes through each letter and checks if the guessed letter is in word then display letter if correct
+# Goes through each letter and checks if the guessed letter is in word then display letter if correct, displays blank if incorrect
     def check_letters(self):
+        word_guessed = True
         self._blanked_word = ""
         for letter in self._hangman_word:
             if letter in self._guesses:
                 self._blanked_word += letter
             else:
                 self._blanked_word += "_"
+                word_guessed = False
             self._blanked_word += " "
+        # if the whole word has been guessed, then the game ends
+        if word_guessed == True:
+            self._game_over = True
+            print(f"Congratulations, you guessed it! It was \"{self._hangman_word}\".")
 
-  # Goes through each letter and checks if the guessed letter is in word then displays blank if incorrect
+
+    #checks if user's guess letter is correct and updates screen
+    def check_user_guess(self, _guess):
+
+        if _guess in self._hangman_word:
+            # correct guess
+            self._guesses += _guess
+
+        else:
+            #incorrect guess
+            self._incorrect += 1
+            if self._incorrect == 6:
+                self._game_over = True
+                print("GAME OVER!")
+                print(f"Sorry, you lost. The word was \"{self._hangman_word}\".")
+
 
 # starts the game
     def start_game(self):
         #generates random word
         self._hangman_word = self.generate_word()
+        self.check_letters()
+
         # while game is playing
         while self._game_over == False:
-            self.check_letters()
             print(self._blanked_word)
-            user_letter = input ("Try guessing a letter.")
-            self._guesses += user_letter
-            
-            
-            #wip, still need to add number of guesses & set game over, plus other things.
+            user_letter = input ("Try guessing a letter.\n").lower() 
+            if len(user_letter) == 1:
+                self.check_user_guess(user_letter)
+            else:
+                print("Please only select one letter.")
+            self.check_letters()
+
+        # remove spaces using strip from input
