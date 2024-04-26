f4247fc8e656d30a4154f47e49acc5099d596c1d
Kate Gerber <kate@gerber.id.au>
2024-04-26 16:31:57 +1000

    added clearing screen after user input and returning to main menu function

--- src/hangman_functions.py
+++ src/hangman_functions.py
@@ -1,5 +1,10 @@
 import os
 
+def any_key_return_to_menu():
+    # Wait for the user to input a key to acknowledge they have finished the help page
+    user_input = input("Press any key to return to main menu.")
+    # Clears the screen before returning to menu
+    os.system ("clear")
 
 def display_intro():
     # this will clear the screen between menus
@@ -10,15 +15,12 @@ def display_intro():
 def display_help():
     os.system ("clear")
     print("This will print some help info for how to play the game.") 
-    #Wait for the user to input a key to acknowledge they have finished the help page
-    user_input = input("Press any key to return to main menu.")
-    os.system ("clear")
+    any_key_return_to_menu()
 
 def display_stats():
     os.system ("clear")
     print("Stats")
-    user_input = input("Press any key to return to main menu.")
-    os.system ("clear")
+    any_key_return_to_menu()
 
 def play_game():
     os.system("clear")
