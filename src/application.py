d0b4c585453a7027519072725363b9db4b6f44a6
Kate Gerber <kate@gerber.id.au>
2024-04-26 16:04:47 +1000

    made it so both lowercase and uppercase input is accepted

--- src/application.py
+++ src/application.py
@@ -2,6 +2,7 @@
 from hangman_functions import display_help
 
 def create_menu():
+    print( "Welcome to Hangman! \n")
     print("Press 'Space Key' to start game \n") 
     print("Press S to view stats \n")  
     print("Press ? to display help \n") 
@@ -9,20 +10,19 @@ def create_menu():
  
 
     menu_selection = input("Select one of the above and hit enter.")
-    return menu_selection
+    return menu_selection.lower()
 
 user_selection = ""
 
-while user_selection != "Q":
+while user_selection != "q":
     user_selection = create_menu()
-
     if (user_selection == " "):
         print("Start game!")
-    elif (user_selection == "S"):
+    elif (user_selection == "s"):
         print("View stats")
     elif (user_selection == "?"):
         display_help()
-    elif (user_selection == "Q"):
+    elif (user_selection == "q"):
         print("Bye!")
     
     else:
--- src/hangman_functions.py
+++ src/hangman_functions.py
@@ -1,5 +1,13 @@
+import os
+
 # Function to display help info
 def display_help():
+    #this will clear the screen between menus
+    os.system ("clear")
     print("This will print some help info for how to play the game.") 
     #Wait for the user to input a key to acknowledge they have finished the help page
     user_input = input("Press any key to return to main menu.")
+    os.system ("clear")
+
+def play_game():
+    print("")
