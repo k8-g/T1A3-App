49aca948d227ea32e75ce04a6b130c6bd2050435
Kate Gerber <kate@gerber.id.au>
2024-04-27 21:13:21 +1000

    added linebreaks in game

--- src/Game.py
+++ src/Game.py
@@ -16,7 +16,7 @@ class Game:
 
     # generates random word
     def generate_word(self):
-        return self._word_generator.word(word_min_length=5, word_max_length=10)
+        return self._word_generator.word(word_min_length=5, word_max_length=8)
 
     # Getter
     def get_blanked_word(self):
@@ -38,21 +38,30 @@ class Game:
         if word_guessed == True:
             self.win_game()
     
+    def line_break(self):
+        print("---------------------------------------------------------")
+
+
     # win game function
     def win_game(self):
         self._game_over = True
         print(f"Congratulations, you guessed it! It was \"{self._hangman_word}\".")
+        self.line_break()
         self._stats.add_win()
+        
+
 
     # lose game function
     def lose_game(self):
         self._game_over = True
+        self.line_break()
         print("GAME OVER!")
         print(f"Sorry, you lost. The word was \"{self._hangman_word}\".")
         self._stats.add_loss()
 
     #checks if user's guess letter is correct and updates screen
     def check_user_guess(self, _guess):
+        self.line_break()
         # if they guess a letter they've already guessed, this warning comes up and still counts as a turn
         if _guess in self._guesses:
             print(f"You've already guessed {_guess}.")
@@ -66,7 +75,9 @@ class Game:
             print(f"Sorry, {_guess} isn't in this word.")
             if self._incorrect == 6:
                 self.lose_game()
-        print("____________________________________________________________")
+        else:
+            print("Yay! You guessed correct!")   
+        self.line_break()
 
     # tells when to draw what hangman stage
     def draw_hangman(self):
@@ -101,8 +112,10 @@ class Game:
             if len(user_guess) == 1:
                 self.check_user_guess(user_guess)
             else:
+                self.line_break()
                 print("Please only select one letter.")
+                self.line_break()
+
             self.check_letters()
             self.draw_hangman()
-        print("____________________________________________________________")
-
+        self.line_break()
