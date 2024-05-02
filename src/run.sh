6d95e29dd8d392ea94c09bade17b4c6180c2abbb
Kate Gerber <kate@gerber.id.au>
2024-05-02 13:53:16 +1000

    Added a bash script to run all requirements for the program and the program itself

--- src/run.sh
+++ src/run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# Make the scripts executable
+chmod +x ./check_python.sh
+chmod +x ./create_venv.sh
+chmod +x ./install_packages.sh
+# Run the check python script
+./check_python.sh
+
 # Check these are installed before running
-python3 -m venv .venv
-source .venv/bin/activate
+./create_venv.sh
 
 # Packages to be installed
-pip3 install pytest
-# pip3 install colored
-pip3 install wonderwords
-pip3 install rich
+./install_packages.sh
 
 # Runs the program
 python3 hangman.py
